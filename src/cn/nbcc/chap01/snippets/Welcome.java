9240a327cf6848267be64c75f0a979717216e970
zhengzhe <zhe_zheng@126.com>
2013-12-02 16:19:50 +0800

    add chap01 snippet
    Welcome

---
+++ src/cn/nbcc/chap01/snippets/Welcome.java
@@ -0,0 +1,18 @@
+package cn.nbcc.chap01.snippets;
+/**
+ * 
+ * @author  zhengzhe
+ * @2013-12-2
+ */
+public class Welcome {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+
+	}
+
+}
+
